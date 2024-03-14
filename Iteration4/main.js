b263e6abe364013d17d2a2bb1b1d624a5eef66a7
Lizok-Tikhonova <tliza5092@gmail.com>
2024-03-14 11:27:48 +0300

    arithmetic operations

---
+++ Iteration4/main.js
@@ -0,0 +1,37 @@
+
+let button = document.querySelector('.ok')
+
+button.onclick = (e)=>{
+    // e.preventDefault()
+    let result = document.querySelector('.result')
+    let num1 = +document.querySelector('.num1').value
+    let num2 = +document.querySelector('.num2').value
+    let symbol = document.querySelector('.symbol').value
+
+    try{
+        if(!num1 && !num2){ 
+            result.textContent =  `Результат`
+            throw new Error("Введены не числа");  
+        }
+            if(symbol == '+'){
+                result.textContent =  `Результат ${num1+num2}`
+            } else if(symbol == '-'){
+                result.textContent =  `Результат ${num1-num2}`
+            } else if(symbol == '*'){
+                result.textContent =  `Результат ${num1*num2}`
+            } else if(symbol == '/'){
+                result.textContent =  `Результат ${num1/num2}`
+            } else{
+                alert('Считаются только арифметические операции')
+                result.textContent =  `Результат = `
+            }   
+    } catch(e){
+        alert('Error: '+ e.message)
+    }
+    
+    //  else {
+    //     alert('Введите числа')
+    //     result.textContent =  `Результат`
+    // }
+       
+}
